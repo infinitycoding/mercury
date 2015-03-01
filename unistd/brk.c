2a02aa1c5414ab76aa2785dc43c2c8fef9364500
Micha <micha@infinitycoding.de>
2015-03-01 22:19:31 +0100

    brk & sbrk

---
+++ unistd/brk.c
@@ -0,0 +1,20 @@
+
+#include <unistd.h>
+#include <stdint.h>
+#include <universe.h>
+
+static void *heap_end = 0;
+
+int brk(void *addr)
+{
+    heap_end = (void*) linux_syscall(SYS_BRK, addr, 0, 0, 0, 0);
+    return 0;
+}
+
+void *sbrk(intptr_t increment)
+{
+	brk(0);
+    int r = brk(heap_end + increment);
+    return (r < 0) ? r : heap_end;
+}
+
