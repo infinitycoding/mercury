db4f8e14deb4860946b67c07061258cbc9432e67
Simon <simon.diepold@infinitycoding.de>
2017-07-26 23:14:58 +0200

    moved architecture depend files

--- building/linux/I386.mk
+++ building/linux/I386.mk
@@ -4,10 +4,9 @@ CXX = g++
 AS  = nasm
 LD  = ld
 
-AS_SRCS    += $(shell find -path './sys/linux/I386/*.asm') 
+AS_SRCS    += $(shell find -path './sys/linux/I386/*.asm')
 C_SRCS     += $(shell find -path './sys/linux/I386/*.c')
-CFLAGS     += -m32 -Iinclude/linux
-CXXFLAGS   += -m32 -Iinclude/linux
+CFLAGS     += -m32 -Iinclude/sys/linux/I386/
+CXXFLAGS   += -m32 -Iinclude/sys/linux/I386/
 LIBC_PATH   = $(LIBEXT)linux_libc.a
 LIBCXX_PATH = $(LIBEXT)linux_libc++.a
-
--- building/noos/AMD64.mk
+++ building/noos/AMD64.mk
@@ -1,11 +1,8 @@
 
-CC  = gcc
+CC  = clang
 CXX = clang++
 AS  = nasm
 LD  = ld
-
-CFLAGS     += -m32
-CXXFLAGS   += -m32
 
 C_SRCS      = $(shell find -name '*.c' ! -path './sys/*' ! -path './test/*' ! -path './stdio/osdep/*' ! -path './stdlib/malloc.c'  $(C_SRC_SEARCH_EX))
 C_TEST_SRC  = $(shell find -name '*.c' -path './test/noos/*' )
