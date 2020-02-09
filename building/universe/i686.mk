7edd5c4383b88115cbcec96b96a63153225f3d1c
tdotu <simon.diepold@infinitycoding.de>
2020-02-09 16:58:23 +0000

    fixed including

--- building/universe/i686.mk
+++ building/universe/i686.mk
@@ -5,9 +5,8 @@
 #LD  = ld
 
 AS_SRCS    += $(shell find -path './sys/universe/I686/*.asm')
-C_SRCS      = $(shell find -name '*.c' ! -path './sys/*' ! -path './test/*' ! -path './stdio/osdep/*' $(C_SRC_SEARCH_EX))
-C_SRCS     += $(shell find -path './sys/universe/I686/*.c' )
-CFLAGS     += -m32 -Iinclude/sys/universe/I686/
-CXXFLAGS   += -m32 -Iinclude/sys/universe/I686/
+C_SRCS      = $(shell find -name '*.c' ! -path './sys/*' ! -path './test/*' ! -path './stdio/osdep/*' $(C_SRC_SEARCH_EX)) $(shell find -path './sys/universe/i686/*.c' )
+CFLAGS     += -m32 -Iinclude/sys/universe/i686/
+CXXFLAGS   += -m32 -Iinclude/sys/universe/i686/
 LIBC_PATH   = $(LIBEXT)universe_libc.a
 LIBCXX_PATH = $(LIBEXT)universe_libc++.a
